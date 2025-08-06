662dc6ad52fcd787c07f0137662f551023ee2b53
Kerstin Bauer <kerstin.bauer@infofauna.ch>
2025-08-06 17:00:10 +0100

    update map with geojson, copy button, swisstopo map

--- leaflet_micah/script.js
+++ leaflet_micah/script.js
@@ -1,9 +1,7 @@
-let map = L.map('map').setView([46.8, 8.2], 8)
+let map = L.map('map', { crs: L.CRS.EPSG2056 }).setView([1184616, 2660019], 15)
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+L.tileLayer.swiss().addTo(map);
+;
 
 let drawItems = new L.FeatureGroup();
 map.addLayer(drawItems);
@@ -26,7 +24,7 @@ let drawControl = new L.Control.Draw({
 map.addControl(drawControl)
 
 // function to display coordinates in JSON format 
-//KB: modified structure
+//KB: modified structure of marker and polyline, the others are still the originals, but set to fals so they are not loaded
 
 function displayCoordinates(layer) {
       let coordinates = [];
@@ -47,14 +45,14 @@ function displayCoordinates(layer) {
                   coordinates.push({ lat: point.lat, lng: point.lng });
             })
       } else if (layer instanceof L.Polyline) {
-            featureType = "Polyline";
+            featureType = "LineString";
             layer.getLatLngs().forEach(function (point) {
-                  coordinates.push({ lat: point.lat, lng: point.lng });
+                  coordinates.push([point.lng,point.lat ]);
             });
       } else if (layer instanceof L.Marker) {
-            featureType = "Marker";
+            featureType = "Marker"; 
             let latLng = layer.getLatLng();
-            coordinates.push({ lat: latLng.lat, lng: latLng.lng })
+            coordinates.push([latLng.lng, latLng.lat ])
       } else if (layer instanceof L.CircleMarker) {
             featureType = "CircleMarker";
             let latLng = layer.getLatLng();
@@ -62,12 +60,28 @@ function displayCoordinates(layer) {
       }
 
       let featureInfo = {
-            type: featureType,
-            coordinates: coordinates
+            type: "Feature",
+			geometry: {
+				type: featureType,
+				coordinates: coordinates}
       }
 
       document.getElementById('coordinates').textContent = JSON.stringify(featureInfo, null, 2);
       console.log("Feature information:", featureInfo);
+}
+
+function CopyToClipboard(id){
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    try {
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+        console.log('Successfully copy text: hello world ' + r);
+    } catch (err) {
+        console.log('Unable to copy!');
+    }
 }
 
 map.on('draw:created', function (event) {
