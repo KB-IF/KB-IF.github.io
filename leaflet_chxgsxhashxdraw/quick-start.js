753f4e76de9314d7658166451c3e7e391b9d48e6
Kerstin Bauer <kerstin.bauer@infofauna.ch>
2026-01-12 11:20:31 +0000

    upload leaflet mashup

---
+++ leaflet_chxgsxhashxdraw/quick-start.js
@@ -0,0 +1,301 @@
+// Create map and attach id to element with id "mapid"
+var map = L.map('mapid', {
+    minZoom: 13,
+    maxZoom: 28,
+  // Use LV95 (EPSG:2056) projection
+  crs: L.CRS.EPSG2056,
+});
+
+// Add Swiss layer with default options
+L.tileLayer.swiss().addTo(map);
+
+// Center the map on Switzerland
+map.fitSwitzerland();
+
+// Add a marker with a popup in Bern
+//L.marker(L.CRS.EPSG2056.unproject(L.point(2600000, 1200000))).addTo(map)
+//  .bindPopup('Bern')
+//  .openPopup();
+//  
+// Add geoserver layers
+//var wmsLayer = L.Geoserver.wms("https://geo.infofauna.ch/geoserver/wms", {
+//  layers: "zsdb:zsdb_sites_inactive",
+//  maxNativeZoom: 25,
+//  maxZoom: 28
+//});
+//wmsLayer.addTo(map);
+
+
+// PLUGIN HASH AND COORDINATES
+
+'use strict';
+
+var NARROW_NO_BREAK_SPACE = '\u202F';
+
+// Initialize Leaflet-hash plugin which syncs map center/zoom with URL hash
+var hash = L.hash(map);
+hash.update();
+
+if (map.getZoom() === undefined) {
+  // Set default view if there was no URL hash
+  map.setView(L.CRS.EPSG2056.unproject(L.point(2600000, 1200000)), 21);
+}
+
+// Add scale bar on the bottom left
+L.control.scale({ imperial: false, maxWidth: 200 }).addTo(map);
+
+
+//To stop popup from showing up when drawing on map.
+var drawing = false;
+map.on(L.Draw.Event.DRAWSTART, e => drawing = true);
+map.on(L.Draw.Event.DRAWSTOP, e => drawing = false);
+
+console.log(L.Draw.Event);
+
+addCoordinatesPopupOnClick(map);
+addMouseMoveCoordinates(map);
+
+function addCoordinatesPopupOnClick(map) {
+  new ClipboardJS('button').on('success', function (e) {
+    var element = e.trigger;
+    element.classList.add('tooltipped', 'tooltipped-s', 'tooltipped-no-delay');
+    element.setAttribute('aria-label', 'Copied!');
+    var removeTooltip = function() {
+      element.classList.remove('tooltipped', 'tooltipped-s', 'tooltipped-no-delay');
+      element.removeAttribute('aria-label');
+    };
+    element.addEventListener('mouseleave', removeTooltip, { once: true });
+    element.addEventListener('blur', removeTooltip, { once: true });
+  });
+  map.on('click', function (event) {
+	if(drawing) return; //if drawing of polygons is active, do not produce popup
+    var latlng = event.latlng;
+    var popup = L.popup(latlng, {
+      content: '<div style="width: 250px;">' +
+        renderCoordinates({
+          label: 'LV95 (E, N)',
+          id: 'coordinates-lv95',
+          displayText: formatLv95Coordinates(latlng, NARROW_NO_BREAK_SPACE),
+          clipboardText: formatLv95Coordinates(latlng, '')
+//        }) +
+//        renderCoordinates({
+//          label: 'WGS 84 (latitude, longitude)',
+//          id: 'coordinates-wgs84',
+//          displayText: formatWgs84Coordinates(latlng, true),
+//          clipboardText: formatWgs84Coordinates(latlng, false)
+        }) +
+        '</div>'
+    });
+    popup.openOn(map);
+  });
+}
+
+function renderCoordinates(options) {
+  return '' +
+    '<div class="form-group">' +
+    '  <div class="form-group-header"><label for="' + options.id + '">' + options.label + '</label></div>' +
+    '  <div class="form-group-body">' +
+    '    <div class="input-group">' +
+    '      <input type="text" readonly id="' + options.id + '" class="form-control" onfocus="replaceWithClipboardText(this)"' +
+    '             value="' + options.displayText + '" data-clipboard-text="' + options.clipboardText + '">' +
+    '      <span class="input-group-button">' +
+    '        <button class="btn-octicon" data-clipboard-text="' + options.clipboardText + '" aria-label="Copy to clipboard">' +
+    '          <img src="img/copy.svg" alt="Copy to clipboard">' +
+    '        </button>' +
+    '      </span>' +
+    '    </div>' +
+    '  </div>' +
+    '</div>';
+}
+
+function replaceWithClipboardText(textInput) {
+  var value = textInput.value;
+  var clipboardText = textInput.dataset.clipboardText;
+  if (clipboardText !== value) {
+    textInput.value = clipboardText;
+    textInput.select();
+    textInput.addEventListener('blur', function() {
+      textInput.value = value;
+    }, { once: true });
+  }
+}
+
+function addMouseMoveCoordinates(map) {
+  var MouseMoveCoordinatesControl = L.Control.extend({
+    onAdd: function (map) {
+      var container = L.DomUtil.create('div', 'leaflet-control-mouse-move-coordinates');
+      container.style.background = 'rgba(255, 255, 255, 0.9)';
+      container.style.border = '2px solid #777';
+      container.style.padding = '2px 4px';
+      container.style.color = '#333';
+      container.style.fontFeatureSettings = 'tnum';
+      var updateCoordinates = function (latlng) {
+        var formattedCoordinates = formatLv95Coordinates(latlng, NARROW_NO_BREAK_SPACE)
+        container.innerHTML = '<b>LV95 Coordinates (E, N)</b><br>' + formattedCoordinates;
+      };
+      map.on('mousemove', function (event) {
+        updateCoordinates(event.latlng);
+      });
+      updateCoordinates(map.getCenter());
+      return container;
+    }
+  });
+
+  new MouseMoveCoordinatesControl().addTo(map);
+}
+
+function formatLv95Coordinates(latlng, separator) {
+  var EN = L.CRS.EPSG2056.project(latlng);
+  var coordinates = [EN.x, EN.y];
+  var formattedCoordinates = coordinates.map(function(coordinate) {
+    var parts = [];
+    coordinate = String(Math.round(coordinate));
+    while (coordinate) {
+      parts.unshift(coordinate.slice(-3));
+      coordinate = coordinate.slice(0, -3);
+    }
+    return parts.join(separator);
+  });
+  return formattedCoordinates.join(', ');
+}
+
+function formatWgs84Coordinates(latlng, addSuffix) {
+  var coordinates = [latlng.lat, latlng.lng];
+  var suffixes = ['°N', '°E'];
+  var formattedCoordinates = coordinates.map(function(coordinate, i) {
+    var formatted = coordinate.toFixed(5);
+    if (addSuffix) {
+      formatted += suffixes[i];
+    }
+    return formatted;
+  });
+  return formattedCoordinates.join(', ');
+}
+
+// GEOSERVER WMS, WFS
+
+var wfsLayer = L.Geoserver.wfs("https://geo.infofauna.ch/geoserver/wfs", {
+  layers: "zsdb:ZSDB_bek_KS",
+  style: {
+	  color: "#A80000",
+//	  fillOpacity: "0",
+//	  opacity: "0.5",
+	  fillColor: "#FF5500",
+	  },
+  onEachFeature: function (feature, layer){
+	  layer.bindPopup("ZSDB site: " + feature.properties.Name);
+}});
+wfsLayer.addTo(map);
+
+var wfsLayer = L.Geoserver.wfs("https://geo.infofauna.ch/geoserver/wfs", {
+  layers: "zsdb:zsb_sites_potentiels_rail_mysql",
+  style: {
+	  color: "#E31AEO",
+//	  fillOpacity: "0",
+//	  opacity: "0.5",
+	  fillColor: "#E31AEO",
+	  },
+  onEachFeature: function (feature, layer){
+	  layer.bindPopup("ZSDB site: " + feature.properties.Name);
+}});
+wfsLayer.addTo(map);
+
+// DRAW
+let drawItems = new L.FeatureGroup();
+map.addLayer(drawItems);
+
+// initialize the draw control
+
+let drawControl = new L.Control.Draw({
+      edit: {
+            featureGroup: drawItems
+      },
+      draw: {
+            polygon: false,
+            polyline: true,
+            rectangle: false,
+			circle: false,
+            circlemarker: false,
+            marker: false
+      }
+})
+map.addControl(drawControl)
+
+// function to display coordinates in JSON format 
+//KB: modified structure of marker and polyline, the others are still the originals, but set to fals so they are not loaded
+
+function displayCoordinates(layer) {
+      let coordinates = [];
+      let featureType = "";
+
+      if (layer instanceof L.Polygon) {
+            featureType = "Polygon";
+            layer.getLatLngs()[0].forEach(function (point) {
+                  coordinates.push({ lat: point.lat, lng: point.lng });
+            })
+      } else if (layer instanceof L.Circle) {
+            featureType = "Circle";
+            let latLng = layer.getLatLng();
+            coordinates.push({ lat: latLng.lat, lng: latLng.lng });
+      } else if (layer instanceof L.Rectangle) {
+            featureType = "Rectangle";
+            layer.getLatLngs()[0].forEach(function (point) {
+                  coordinates.push({ lat: point.lat, lng: point.lng });
+            })
+      } else if (layer instanceof L.Polyline) {
+            featureType = "LineString";
+            layer.getLatLngs().forEach(function (point) {
+				  let xy = L.CRS.EPSG2056.project(point);
+                  coordinates.push([xy.x, xy.y]);
+            });
+      } else if (layer instanceof L.Marker) {
+            featureType = "Marker"; 
+            let latLng = layer.getLatLng();
+            coordinates.push([latLng.lng, latLng.lat ])
+      } else if (layer instanceof L.CircleMarker) {
+            featureType = "CircleMarker";
+            let latLng = layer.getLatLng();
+            coordinates.push({ lat: latLng.lat, lng: latLng.lng })
+      }
+	  else {
+		  console.log('Unknown layer type');
+		  console.log(layer);
+		  console.log(layer.constructor.name);
+	  }
+
+      let featureInfo = {
+            type: "Feature",
+			geometry: {
+				type: featureType,
+				coordinates: coordinates}
+      }
+
+      document.getElementById('coordinates').textContent = JSON.stringify(featureInfo, null, 2);
+      console.log("Feature information:", featureInfo);
+}
+
+function CopyToClipboard(id){
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    try {
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+        console.log('Successfully copy text: hello world ' + r);
+    } catch (err) {
+        console.log('Unable to copy!');
+    }
+}
+
+map.on(L.Draw.Event.CREATED, function (event) {
+	
+    let layer = event.layer;
+    drawItems.clearLayers(); //Ensures that only one drawing can exist on map
+	drawItems.addLayer(layer);
+    displayCoordinates(layer);
+})
+
+map.on(L.Draw.Event.EDITED, function (event) {
+	event.layers.eachLayer(layer => displayCoordinates(layer));
+})
---
+++ leaflet_chxgsxhashxdraw/read_me.txt
@@ -0,0 +1,15 @@
+- Swiss Tile layer
+- geoserver plugin
+- hash plugin for CH coordinates --> creates popup with point coordinates and "copy to clipboard" - but doesn't show svp icon
+	hash plugin allows to display coordinates after hash in URL to return to the URL's zoom level and coordinates
+- out-commented coordinates of WGS in popup
+- also added a scale bar on the bottom left
+
+---
+in addition:
+- added draw plugin and adaptions from micah map
+- disabled marker placement because (swiss) coordinates can be read and copied with the hash plugin
+- unfortunately the hash plugin popup is in the way when placing a line (fixed)
+- line is still not in Swiss coordinates (fixed)
+- to check: display existing ZSDB sites in map that will be included in iframe in the site registration form?
+works 17.11.2025, fixed 08.01.2016
